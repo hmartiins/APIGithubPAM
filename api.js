b61c628279b2510a9399a4194f055b4444011fb4
hmartiins <hmartins224@gmail.com>
2020-08-21 21:49:45 -0300

    Starting the development of integration with api from github

---
+++ api.js
@@ -0,0 +1,12 @@
+const usernameGit = document.getElementById('usernameGit');
+const gitImage = document.getElementById('gitImage');
+
+const url = "https://api.github.com/users/"
+
+async function getData(){
+	try {
+		
+	} catch (err) {
+		
+	}
+}
