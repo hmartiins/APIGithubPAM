4844870eae1dac7de7a07bc48c00a5c740ef0607
hmartiins <hmartins224@gmail.com>
2020-08-21 23:24:16 -0300

    null name error handling

--- api.js
+++ api.js
@@ -1,4 +1,3 @@
-
 const url = "https://api.github.com/users";
 
 const clientId = "ea8b8ebd33c33237a470";
@@ -7,9 +6,10 @@ const clientSecret = "ccd56e1f252f5f94deafc82bb570125d5868e0e1";
 async function getData(){
 	const usernameGit = document.getElementById('usernameGit').value;
 
-
-	console.log('oi');
-	console.log(usernameGit);
+	if(!usernameGit > 0){
+		alert('Digite algum username !!')
+		return;
+	}
 	try {
 		const profile = await fetch(
 			`${url}/${usernameGit}?client_id=${clientId}&client_secret=${clientSecret}`
