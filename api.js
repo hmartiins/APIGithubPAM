af5139737638e646d62b7f60f41aa14112abeb77
hmartiins <hmartins224@gmail.com>
2020-08-21 23:55:57 -0300

    Creating user html template

--- api.js
+++ api.js
@@ -17,8 +17,62 @@ async function getData(){
 
 		const profileJson = profile.json();
 
-		console.log(profileJson);
+		return profileJson;
 	} catch (err) {
 		console.log(err);
 	}
+}
+
+function showProfileUser(){
+	const user = getData();
+	console.log(user.blog);
+	const profileShow = document.getElementById('cont');	
+
+	profileShow.innerHTML = `<div class="row">
+	<div class="col-10 offset-1 mt-5 text-center">
+		<img
+			src=${user.avatar_url}
+			alt="Imagem de perfil"
+			class="img-thumbnail rounded"
+			id="gitImage"
+		/>
+	</div>
+	</div>
+
+	<div class="row">
+	<div class="col-12 mt-3 d-flex justify-content-center">
+		<ul class="list-group list-group-flush text-light">
+			<li class="list-group-item bg-dark">
+				<b>Nome: ${user.name}</b>
+			</li>
+			<li class="list-group-item bg-dark">
+				<b>bio:</b>
+				<p>
+					${user.name}
+				</p>
+			</li>
+			<li class="list-group-item bg-dark">
+				<b>Seguidores: ${user.followers}</b>
+			</li>
+			<li class="list-group-item bg-dark">
+				<b>Seguindo: ${user.following}</b>
+			</li>
+			<li class="list-group-item bg-dark">
+				<b>Stars: ${user.stars}</b>
+			</li>
+			<li class="list-group-item bg-dark">
+				<b>Repositórios: ${user.public_repos}</b>
+			</li>
+			<li class="list-group-item bg-dark">
+				<a
+					href="${user.html_url}"
+					class="btn btn-block btn-primary"
+					target="blank"
+				>
+					Visitar Perfil
+				</a>
+			</li>
+		</ul>
+	</div>
+	</div>`
 }
